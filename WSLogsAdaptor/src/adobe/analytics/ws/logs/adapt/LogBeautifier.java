31b665ba5a7ed4acd4670b7b4bcc2900c87d6710
Vishwas Kumar Singh <vishwassin@gmail.com>
2016-07-19 18:14:46 +0530

    checkin
    checkin

--- WSLogsAdaptor/src/adobe/analytics/ws/logs/adapt/LogBeautifier.java
+++ WSLogsAdaptor/src/adobe/analytics/ws/logs/adapt/LogBeautifier.java
@@ -1,10 +1,25 @@
 package adobe.analytics.ws.logs.adapt;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LogBeautifier {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try (BufferedReader br = new BufferedReader(new FileReader("C:/Users/vishwsin/Desktop/work/Dallas_19_Requests.log")))
+		{
 
+			String sCurrentLine;
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
 	}
 
 }
